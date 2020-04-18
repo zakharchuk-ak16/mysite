2ec59746b53676f4ee1e6f8a27c13f29b7b1d382
zakharchuk-ak16 <zakharchuk_ak16@nuwm.edu.ua>
2020-04-18 16:54:51 +0300

    lab8

---
+++ mysite/app_blog/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+# app_blog /views.py
+from django.shortcuts import render
+from django.views.generic import TemplateView
+# Створіть свої представлення тут.
+class HomePageView(TemplateView):
+    def get(self, request, **kwargs):
+        return render(request, 'index.html', context=None)
