08a53c1516bdd8d06a268b0d2967394366f4602f
zakharchuk-ak16 <zakharchuk_ak16@nuwm.edu.ua>
2020-04-28 15:20:14 +0300

    Lab9

--- mysite/app_blog/admin.py
+++ mysite/app_blog/admin.py
@@ -1,3 +1,48 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
+from django.shortcuts import get_object_or_404
+from .models import Article, ArticleImage, Category
+from .forms import ArticleImageForm
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('category',)
+    fieldsets = (
+        ('', {
+            'fields': ('category', ),
+        }),
+    )
+admin.site.register(Category, CategoryAdmin)
+class ArticleImageInline(admin.TabularInline):
+    model = ArticleImage
+    form = ArticleImageForm
+    extra = 0
 
-# Register your models here.
+    fieldsets = (
+        ('', {
+            'fields': ('title', 'image',),
+        }),
+    )
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'pub_date', 'slug', 'main_page')
+    inlines = [ArticleImageInline]
+    multiupload_form = True
+    multiupload_list = False
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('category',)
+    fieldsets = (
+                    ('', {
+                        'fields': ('pub_date', 'title', 'description',
+                                   'main_page'),
+                    }),
+                    ((u'Додатково'), {
+                        'classes': ('grp-collapse grp-closed',),
+                        'fields': ('slug',),
+                    }),
+    )
+
+    def delete_file(self, pk, request):
+        '''Delete an image.'''
+        obj = get_object_or_404(ArticleImage, pk=pk)
+        return obj.delete()
+
+
+admin.site.register(Article, ArticleAdmin)
